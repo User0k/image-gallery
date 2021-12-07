1a743eef6d5534bb5b56fe02e72eb9af90b6021d
User0k <glucus2@yandex.ru>
2021-12-07 21:26:50 +0300

    feat: get data from api with axios and dotenv, return container from mui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
+import { Container } from "@mui/material";
+import axios from "axios";
+
+
 function App() {
+  const getData = async () => {
+    const res = await axios.get(`https://api.unsplash.com/photos/random/?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`);
+    const dataRes = await res.data;
+    console.log(dataRes);
+  }
+
+  useEffect(() => getData());
+
   return (
-    <div>
-      Something
-    </div>
+    <Container>Something</Container>
   );
 }
 
