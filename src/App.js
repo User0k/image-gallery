a2c4142b3f060331d3727a1c63a15482af8870e0
User0k <glucus2@yandex.ru>
2021-12-21 23:51:00 +0300

    refactor: apply constant url and change get request to promise-form

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Gallery from './componenta/Gallery';
 
 function App() {
   const [images, setImages] = useState([]);
+  const url = `https://api.unsplash.com/search/photos?page=1&query=fox&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}&per_page=20`;
 
-  const getData = async () => {
-    const res = await axios.get(
-      `https://api.unsplash.com/search/photos?page=1&query=fox&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}&per_page=25`
-    );
-    const dataRes = await res.data;
-    setImages(dataRes.results);
+  const getData = () => {
+    axios.get(url)
+      .then(res => {
+        setImages(res.data.results);
+      });
   };
 
   useEffect(() => getData());
