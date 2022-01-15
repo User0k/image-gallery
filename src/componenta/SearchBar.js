b54b02150036d5b482c3b704ec5dbf2be339c349
User0k <glucus2@yandex.ru>
2022-01-15 18:06:39 +0300

    feat: add logo and search button, make SearchBar more responsive

--- src/componenta/SearchBar.js
+++ src/componenta/SearchBar.js
@@ -3,10 +3,14 @@ import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import InputBase from '@mui/material/InputBase';
+import Box from '@mui/material/Box';
 import CameraEnhanceIcon from '@mui/icons-material/CameraEnhance';
 import SearchIcon from '@mui/icons-material/Search';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
 
-const Search = styled('div')(({ theme }) => ({
+const Search = styled('form')(({ theme }) => ({
+  display: 'flex',
   position: 'relative',
   width: '100%',
   borderRadius: theme.shape.borderRadius,
@@ -17,20 +21,31 @@ const Search = styled('div')(({ theme }) => ({
 }));
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
+  [theme.breakpoints.down('sm')]: {
+    display: 'none',
+  },
+  [theme.breakpoints.up('sm')]: {
+    display: 'inline-flex',
+  },
   padding: theme.spacing(0, 2),
   height: '100%',
   position: 'absolute',
   pointerEvents: 'none',
-  display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  width: '100%',
   color: 'inherit',
   '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    [theme.breakpoints.down('sm')]: {
+      paddingLeft: '0.7em',
+    },
+    [theme.breakpoints.up('sm')]: {
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    },
     transition: theme.transitions.create('width'),
   },
 }));
@@ -40,8 +55,8 @@ export default function SearchBar() {
     <AppBar sx={{ backgroundColor: 'black' }}>
       <Toolbar>
           <CameraEnhanceIcon
+            sx={{ display: { md: 'inline-block', sm: 'none', xs: 'none' }, mr: 3.3, color: 'white' }}
             fontSize="large"
-            sx={{ mr: 3.3, color: 'white' }}
           />
         <Search>
           <SearchIconWrapper>
@@ -49,8 +64,23 @@ export default function SearchBar() {
           </SearchIconWrapper>
           <StyledInputBase
             placeholder="Search images"
-            inputProps={{ 'aria-label': 'search' }}
+            inputProps={{ 'aria-label': 'search images' }}
           />
+          <Box sx={{ flexGrow: 1 }} />
+          <Button
+            sx={{ display: { sm: 'inline-flex', xs: 'none' } }}
+            variant="contained"
+            size='small'
+            type="submit"
+            aria-label="search">
+              search
+          </Button>
+          <IconButton
+            sx={{ display: { sm: 'none', xs: 'inline-flex' }, color: 'lightgrey' }}
+            type="submit"
+          >
+            <SearchIcon />
+          </IconButton>
         </Search>
       </Toolbar>
     </AppBar>
