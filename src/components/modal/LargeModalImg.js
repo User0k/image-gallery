49a6d7845bf037b8fbab9d16e27fdc55e76288d8
User0k <glucus2@yandex.ru>
2022-02-18 23:45:05 +0300

    feat: add styles for modal

---
+++ src/components/modal/LargeModalImg.js
@@ -0,0 +1,12 @@
+import { styled } from '@mui/material/styles';
+
+const LargeModalImg = styled('img')(() => ({
+  display: 'block',
+  margin: '0 auto',
+  objectFit: 'cover',
+  height: '100%',
+  width: '100%',
+  cursor: 'zoom-out',
+}));
+
+export default LargeModalImg;
---
+++ src/components/modal/SmallModalImg.js
@@ -0,0 +1,12 @@
+import { styled } from '@mui/material/styles';
+
+const SmallModalImg = styled('img')(() => ({
+  display: 'block',
+  margin: '0 auto',
+  objectFit: 'contain',
+  height: '40vw',
+  width: 'auto',
+  cursor: 'zoom-in',
+}));
+
+export default SmallModalImg;
